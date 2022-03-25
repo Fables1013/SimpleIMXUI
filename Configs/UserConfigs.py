b9c3a41e2006839019f31362a2dc452b421c6fa3
kyleeasop <kyle.easop@gmail.com>
2022-03-25 11:15:57 -0400

    UserConfigs comment

--- Configs/UserConfigs.py
+++ Configs/UserConfigs.py
@@ -1,3 +1,13 @@
 
+'''
+Your public and private keys are required to authenticate yourself and fund your purchase as well as receive funds from
+your sales.
+
+These two values are passed directly to the IMXApi (as they would be when using MetaMask and IMXLink) and not saved
+anywhere nor sent to anyone including the authors of this project.
+
+PROVIDING THESE VALUES IS REQUIRED TO USE THIS PROJECT
+'''
+
 privateKey = ""
 publicKey = ""

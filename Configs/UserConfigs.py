1cdbc7b5c0e85687b92807bbf45dd3709e267c38
kyleeasop <kyle.easop@gmail.com>
2022-03-25 11:26:24 -0400

    UserConfigs comment

--- Configs/UserConfigs.py
+++ Configs/UserConfigs.py
@@ -1,12 +1,20 @@
 
 '''
-Your public and private keys are required to authenticate yourself and fund your purchase as well as receive funds from
+Your public and private keys are required to authenticate yourself and fund your purchases as well as receive funds from
 your sales.
 
 These two values are passed directly to the IMXApi (as they would be when using MetaMask and IMXLink) and not saved
-anywhere nor sent to anyone including the authors of this project.
+anywhere nor sent to anyone including the authors of this project. Feel free to look around the project and confirm for
+yourself that this information is only used where required. YOU SHOULD ONLY PROVIDE THIS INFORMATION WHEN YOU FEEL
+COMFORTABLE. YOU CANNOT USE THIS PROJECT UNTIL THESE TWO PIECES OF INFO ARE PROVIDED.
 
-PROVIDING THESE VALUES IS REQUIRED TO USE THIS PROJECT
+I AM NOT RESPONSIBLE IF YOU SHARE YOU PRIVATE KEY WITH ANYONE EITHER INTENTIONALLY OR UNINTENTIONALLY AS A RESULT OF
+USING THIS PROJECT.
+
+FWIW, WHEN USING THIS I LOAD MY PRIVATE KEY FROM A TXT FILE I HAVE SAVED ON MY HARD-DRIVE. YOU CAN MIMIC THAT APPROACH BY
+USING THE BELOW SYNTAX:
+
+privateKey = open('filepath/privateKey.txt', 'r').readline()
 '''
 
 privateKey = ""
